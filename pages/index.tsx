c1e78f2ab3daba6a4ef458774445f1aecbde05d1
Martin d'Hoffschmidt <mdhoffschmidt@protonmail.com>
2022-10-28 19:48:08 +0100

    fix svg bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <Page>
       <h1 className="text-2xl">
-        Welcome to Diod Protocol
+        Welcome to Diode Protocol
       </h1>
     </Page>
   )
